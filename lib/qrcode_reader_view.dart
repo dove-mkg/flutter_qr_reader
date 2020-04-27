f6e826fc14af5b34ba9710a6c41ab21b8edaa7d9
wayne.ye <qwertyuiop17u@gmail.com>
2020-04-27 15:36:25 +0800

    remove scan button

--- lib/qrcode_reader_view.dart
+++ lib/qrcode_reader_view.dart
@@ -185,31 +185,31 @@ class QrcodeReaderViewState extends State<QrcodeReaderView>
                 ),
               ),
             ),
-            Positioned(
-              width: constraints.maxWidth,
-              bottom: constraints.maxHeight == mediaQuery.size.height
-                      ? 12 + mediaQuery.padding.top
-                      : 12,
-              child: 
-                Center(
-                  child: Container(
-                    width: 80,
-                    height: 80,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(40)),
-                      border: Border.all(color: Colors.white30, width: 12),
-                    ),
-                    alignment: Alignment.center,
-                    child: Image.asset(
-                      "assets/tool_qrcode.png",
-                      package: "flutter_qr_reader",
-                      width: 35,
-                      height: 35,
-                      color: Colors.white54,
-                    ),
-                  ),
-                ),
-            )
+            // Positioned(
+            //   width: constraints.maxWidth,
+            //   bottom: constraints.maxHeight == mediaQuery.size.height
+            //           ? 12 + mediaQuery.padding.top
+            //           : 12,
+            //   child: 
+            //     Center(
+            //       child: Container(
+            //         width: 80,
+            //         height: 80,
+            //         decoration: BoxDecoration(
+            //           borderRadius: BorderRadius.all(Radius.circular(40)),
+            //           border: Border.all(color: Colors.white30, width: 12),
+            //         ),
+            //         alignment: Alignment.center,
+            //         child: Image.asset(
+            //           "assets/tool_qrcode.png",
+            //           package: "flutter_qr_reader",
+            //           width: 35,
+            //           height: 35,
+            //           color: Colors.white54,
+            //         ),
+            //       ),
+            //     ),
+            // )
           ],
         );
       }),
